50448e39f858749b62ea888ed7466673374aec7a
nirmallohiya <nlohiya01@gmail.com>
2025-08-12 09:54:11 +0530

    update logo

--- components/footer.jsx
+++ components/footer.jsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 const Footer = () => {
     return (
         <div className="flex justify-around flex-col gap-2 sm:gap-0 sm: flex-row bg-black py-5 items-center">
-            <Image src={assets.logo_light} alt='' width={120} />
+            <Image src={assets.logo} alt='' width={70} />
             <p className="text-sm text-white">All right reserved copyright @blogger</p>
             <div className="flex">
                 <Image src={assets.facebook_icon} alt="" width={40} />
